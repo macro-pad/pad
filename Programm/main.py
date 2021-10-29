139157731a1e982d7a3aa892a85a349ceb3141d3
Zoiiid <59050203+Zoiiid@users.noreply.github.com>
2021-10-29 15:46:05 +0200

    Update main.py

--- Programm/main.py
+++ Programm/main.py
@@ -10,7 +10,7 @@ class ButtonWindow(Gtk.Window):
         hbox = Gtk.Box(spacing=6)
         self.fullscreen()
         self.add(hbox)
-
+        
         button = Gtk.Button.new_with_label("Click Me")
         button.connect("clicked", self.on_click_me_clicked)
         hbox.pack_start(button, True, True, 0)
